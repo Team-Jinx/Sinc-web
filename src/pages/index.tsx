15df7da13fcb38a8b4f9a1755183ec33decd36c5
hyundang <hyunjin5697@yonsei.ac.kr>
2021-12-04 03:11:54 +0900

    fix: index page -> getInitialprops 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import fetcher, { getRefreshToken, setGraphQLClient } from "src/apis";
 import { getQueries } from "src/apis/queries";
 import { TodayArtist, TodayArtist2, TodayArtist3 } from "src/assets/img";
@@ -6,12 +6,40 @@ import { Main } from "src/components/templates";
 import { PfDataQueryProps } from "src/interfaces/PFData";
 import { BannerDataType } from "src/interfaces/types";
 import useSWR from "swr";
-import cookies from "next-cookies";
-import { NextPageContext } from "next";
+// import cookies from "next-cookies";
+import cookie from "react-cookies";
 import { useRecoilValue } from "recoil";
 import states from "src/modules";
+import { useRouter } from "next/router";
 
 const Home = () => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleLogin = async () => {
+      // 토큰
+      const userToken = cookie.load("access-token");
+
+      // 로그인 되어있을 경우
+      if (userToken !== undefined) {
+        // try {
+        await setGraphQLClient(userToken);
+        await getRefreshToken();
+        // await fetcher(getQueries.getUserData());
+        // } catch (e) {
+        //   // 1일 마다 refresh token 발급
+
+        // }
+      }
+      // 로그인 안 된 경우
+      // 로그인 페이지로 이동
+      else {
+        router.push("/login");
+      }
+    };
+
+    handleLogin();
+  }, []);
+
   const [pfDataQuery, setPfDataQuery] = useState<PfDataQueryProps>({
     category: "MUSIC",
   });
@@ -45,29 +73,29 @@ const Home = () => {
 
 export default Home;
 
-Home.getInitialProps = async (ctx: NextPageContext) => {
-  // 토큰
-  const allCookies = cookies(ctx);
-  const userToken = allCookies["access-token"];
+// Home.getInitialProps = async (ctx: NextPageContext) => {
+//   // 토큰
+//   const allCookies = cookies(ctx);
+//   const userToken = allCookies["access-token"];
 
-  // 로그인 되어있을 경우
-  if (userToken !== undefined) {
-    try {
-      await setGraphQLClient(userToken);
-      await fetcher(getQueries.getUserData());
-    } catch (e) {
-      // 1일 마다 refresh token 발급
-      await getRefreshToken();
-    }
-  }
-  // 로그인 안 된 경우
-  // 로그인 페이지로 이동
-  else {
-    ctx.res?.writeHead(307, { Location: "/login" });
-    ctx.res?.end();
-  }
-  return {};
-};
+//   // 로그인 되어있을 경우
+//   if (userToken !== undefined) {
+//     try {
+//       await setGraphQLClient(userToken);
+//       await fetcher(getQueries.getUserData());
+//     } catch (e) {
+//       // 1일 마다 refresh token 발급
+//       await getRefreshToken();
+//     }
+//   }
+//   // 로그인 안 된 경우
+//   // 로그인 페이지로 이동
+//   else {
+//     ctx.res?.writeHead(307, { Location: "/login" });
+//     ctx.res?.end();
+//   }
+//   return {};
+// };
 
 // mock data
 const BannerData: BannerDataType[] = [
