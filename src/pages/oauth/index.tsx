f6c26c040a56fd4a589507ad3bec93d5b3cfa05d
hyundang <hyunjin5697@yonsei.ac.kr>
2021-12-04 03:26:05 +0900

    fix: login page -> getInitialProps 삭제

--- src/pages/oauth/index.tsx
+++ src/pages/oauth/index.tsx
@@ -6,38 +6,40 @@ import { getQueries } from "src/apis/queries";
 import { Loading } from "src/components/templates";
 import states from "src/modules";
 
-interface OAuthProps {
-  kakaoCode: string;
-}
-const OAuth = ({ kakaoCode }: OAuthProps) => {
+// interface OAuthProps {
+//   kakaoCode: string;
+// }
+const OAuth = () => {
   const router = useRouter();
   const setUserData = useSetRecoilState(states.UserDataState);
 
   useEffect(() => {
     const handleLogin = async () => {
-      try {
-        // 로그인 성공
-        const res = await getAccessToken(kakaoCode);
-        await setGraphQLClient(res);
-        const { checkJwt } = await fetcher(getQueries.getUserData());
-        setUserData(checkJwt);
+      if (router.query.code) {
+        try {
+          // 로그인 성공
+          const res = await getAccessToken(String(router.query.code));
+          await setGraphQLClient(res);
+          const { checkJwt } = await fetcher(getQueries.getUserData());
+          setUserData(checkJwt);
 
-        router.replace("/");
-      } catch (e) {
-        // 로그인 실패
-        router.replace("/login");
+          router.replace("/");
+        } catch (e) {
+          // 로그인 실패
+          router.replace("/login");
+        }
       }
     };
 
     handleLogin();
-  }, []);
+  }, [router.query.code]);
 
   return <Loading />;
 };
 
 export default OAuth;
 
-OAuth.getInitialProps = async (ctx: any) => {
-  const kakaoCode = ctx.query.code;
-  return { kakaoCode };
-};
+// OAuth.getInitialProps = async (ctx: any) => {
+//   const kakaoCode = ctx.query.code;
+//   return { kakaoCode };
+// };
